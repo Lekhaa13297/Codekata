30247a40d1c8de9e4d7c414958018f47397c7807
Lekhaa13297 <lekhaarengaraj@gmail.com>
2017-06-22 09:19:47 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,10 @@
+r1=int(input("enter the 1st number"))
+r2=int(input("enter the 2nd number"))
+r3=int(input("enter the 3rd number"))
+if r1>r2 and r1>r3:
+	print("1st one is largest")
+elif r2>r1 and r2>r3:
+	print("2nd one is largest")
+else:
+	print("3rd one is largest")
+	
